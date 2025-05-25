398a2d9d8de72166bcb38521fd3c75310425a387
Yoga Raditya <radityanalaa@gmail.com>
2025-05-25 08:38:38 +0700

    `feat:` Add docker compose

--- db.js
+++ db.js
@@ -1,7 +1,12 @@
 import pg from 'pg';
 
+const POSTGRES_USER = process.env.POSTGRES_USER;
+const POSTGRES_PASSWORD = process.env.POSTGRES_PASSWORD;
+const POSTGRES_DB = process.env.POSTGRES_DB;
+const POSTGRES_HOST = process.env.POSTGRES_HOST;
+
 const pool = new pg.Pool({
-    connectionString: process.env.DSN
+    connectionString: `postgres://${POSTGRES_USER}:${POSTGRES_PASSWORD}@${POSTGRES_HOST}:5432/${POSTGRES_DB}`
 })
 
 export default pool;
--- redis.js
+++ redis.js
@@ -1,6 +1,6 @@
 import { createClient } from 'redis';
 
-const client = createClient();
+const client = createClient({ url: process.env.REDIS_URL });
 client.on('error', (err) => console.log('Redis Client Error', err));
 client.connect();
 
